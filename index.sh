b1e5f23d6d30d08f09de67b93bfeaa55cd12db17
Sanif Naushad <sanif@sofy.ai>
2025-01-16 11:59:57 +0500

    Certificate name handling spaces

--- index.sh
+++ index.sh
@@ -1,8 +1,9 @@
 set -ex
 echo "build path "${build_path}""
 
+
 curl --location --request POST 'https://public.sofy.ai/parser-microservice/build-upload' \
 --header "x-sofy-auth-key: ${subscription_key}" \
 --form "applicationFile=@"${build_path}"" \
 --form "ApplicationGUID="${application_guid}"" \
---form "CertificateName="${certificate_name}""
+--form "CertificateName=${certificate_name}"
